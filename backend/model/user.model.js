bc2ba3504ac6daefb436ee591463e54d2e13b940
Ankitgaund <ankitgaund16@gmail.com>
2024-03-05 12:38:57 +0530

    Created Chatapp to learn making api,cloudinary config and upload,modeling,context api,custome hooks ans socket.io

---
+++ backend/model/user.model.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const {Schema,model}=mongoose;
+const userSchema=new Schema({
+    fullName:{type:String,required:true},
+    username:{type:String,required:true,unique:true},
+    password:{type:String,required:true,minLength:[6,"Password shoud be at least 6 character"]},
+    gender:{type:String,required:true,enum:["male","female"]},
+    profileId:{type:String,default:""},
+    profilePic:{type:String,default:""},
+},{timestamps:true})
+export const User =model("User",userSchema);
